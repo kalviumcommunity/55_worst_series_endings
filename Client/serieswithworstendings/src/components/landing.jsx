73f8718441b5b907cc3c1f71a25e28fe4322fd78
rushikeshzope <rushikesh.zope@gmail.com>
2024-03-04 17:16:20 +0530

    pushed module11

--- Client/serieswithworstendings/src/components/landing.jsx
+++ Client/serieswithworstendings/src/components/landing.jsx
@@ -1,68 +1,42 @@
-import React from 'react';
-import './landing.css';
-import seriesIcon from '../assets/seriesicon.png';
-import howimetyourmother from '../assets/howimetyourmother.jpg';
+    import React from 'react';
+    import './landing.css';
+    import seriesIcon from '../assets/seriesicon.png';
+    import howimetyourmother from '../assets/howimetyourmother.jpg';
 
-function Landing() {
-    return (
-        <div>
-            <nav className="navbar">
-                <img src={seriesIcon} alt="Example" className='tvshowimg' />
-                <span className='navbartext'>Series with worst endings</span>
-                <ul>
-                    <li className="nav-item"><a href="#">Home</a></li>
-                    <li className="nav-item"><a href="#">About page</a></li>
-                    <li className="nav-item"><a href="#">About me</a></li>
-                </ul>
-                <div className="search-container">
-                    <input type="text" placeholder="Search..." className="search-input" />
-                </div>
-            </nav>
-            <div className='translucentcontainer'>
-                <div id="flexItems">
-                    <br />
-                    <div id="searchedSection">
-                        <div className='row1'>
-                        <div className="container">
-                            <img src={howimetyourmother} alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div>
-                        <div className="container">
-                            <img src="./Assets/food.png" alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div>
-                        <div className="container">
-                            <img src="./Assets/food.png" alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div>
-                        <div className="container">
-                            <img src="./Assets/food.png" alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div>
-                        </div>
-                        <div className='row2'>
-                        <div className="container">
-                            <img src="./Assets/food.png" alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div>
-                        <div className="container">
-                            <img src="./Assets/food.png" alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div> 
-                        <div className="container">
-                            <img src="./Assets/food.png" alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div>
-                        <div className="container">
-                            <img src="./Assets/food.png" alt="Food" className="foodSearched" />
-                            <p className="foodnameSearched">Mexican Chicken</p>
-                        </div>
+    function Landing() {
+        return (
+            <div>
+                <nav className="navbar">
+                    <img src={seriesIcon} alt="Example" className='tvshowimg' />
+                    <span className='navbartext'>Series with worst endings</span>
+                    <ul>
+                        <li className="nav-item"><a href="#">Home</a></li>
+                        <li className="nav-item"><a href="#">About page</a></li>
+                        <li className="nav-item"><a href="#">About me</a></li>
+                    </ul>
+                    <div className="search-container">
+                        <input type="text" placeholder="Search..." className="search-input" />
+                    </div>
+                </nav>
+                <div className='translucentcontainer'>
+                    <div id="flexItems">
+                        <br />
+                        <div id="searchedSection">
+                            <div className='row1'>
+                            <div className="container">
+                                <img src={howimetyourmother} alt="Food" className="cardimg" />
+                                <div className='cardtext'>
+                                <p>How i met your mother</p>
+                                <p>Before: 8/10 After:4/10</p>
+                                <p >Seasons: 11</p>
+                                </div>
+                            </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default Landing;
+    export default Landing;
