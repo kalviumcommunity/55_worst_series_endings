336e6d54dda394ba59eacc6d6ed72a015ea44ef6
rushikeshzope <rushikesh.zope@gmail.com>
2024-03-13 14:02:23 +0530

    upated module14 backend

--- Client/serieswithworstendings/src/components/landing.jsx
+++ Client/serieswithworstendings/src/components/landing.jsx
@@ -2,7 +2,7 @@ import './landing.css';
 import seriesIcon from '../assets/seriesicon.png';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom'; // Import Link component from react-router-dom
+import { Link } from 'react-router-dom';
 
 function Landing() {
     const [seriesList, setSeriesList] = useState([]);
